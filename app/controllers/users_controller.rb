e716a4185888f59b27abf2472fdf682085156080
Dhananjay Kumarpatel <dkumarpatel885@gmail.com>
2020-09-05 11:40:54 +0530

    initial commit added all action to the conroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,35 @@ class UsersController < ApplicationController
         )
       render json: user , status: :created
     end
+    
+    def index
+      @users=User.all
+      render json:@users
+    end
+    def show
+      @user=User.find(params[:id])
+      if @user
+        render json:@user
+      else
+        format.json { render json:"User not found" }
+      end
+    end
+
+    def update
+      @user = User.find(params[:id])
+     
+      if @user.update(user_params)
+        render json:@user
+      else
+        format.json { render json:"User not found" }
+      end
+    end
+    def destroy
+      @user=User.find(params[:id])
+      @user.destroy
+    end
+    private
+    def user_params
+      params.require(:user).permit(:username, :email)
+    end
 end
